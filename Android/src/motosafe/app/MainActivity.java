773f67f2d3032cdf5111954c33523272961cb8ec
valensr23 <valen-sr@outlook.com>
2015-01-19 12:38:02 +0100

    BT funciona
    Arreglo del funcionamiento del BT, envia y recibe datos correctamente,
    representandolos en un TextView

--- Android/src/motosafe/app/MainActivity.java
+++ Android/src/motosafe/app/MainActivity.java
@@ -3,22 +3,33 @@ package motosafe.app;
 /*
  * @author: Valentín Sánchez Ramírez
  */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.UUID;
+
+import android.R.string;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -27,31 +38,31 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
  
 public class MainActivity extends Activity {
   private static final String TAG = "bluetooth2";
    
-  Button btnOn, btnOff;
+   
+  //Para el BT
   TextView txtArduino;
-  Handler h;
-   
-  final int RECIEVE_MESSAGE = 1;		// Status  for Handler
-  private BluetoothAdapter btAdapter = null;
-  private BluetoothSocket btSocket = null;
-  private StringBuilder sb = new StringBuilder();
+  BluetoothAdapter bluetooth;
+  Handler puente;
+  BluetoothSocket clientSocket;
+  BluetoothDevice myDevice = null;
+  //SPP UUID service
+  UUID uuid= UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
   
-  private ConnectedThread mConnectedThread;
-   
-  // SPP UUID service
-  private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
- 
-  // MAC-address of Bluetooth module (you must edit this line)
+  
+
+  // MAC-address de mis dispositivo BT
   private static String address = "98:D3:31:B1:CC:72";
   
-  //////////////////////////////////
+
   
 	//TextView de la posicion del GPS
 	private TextView lblLatitud;
@@ -64,10 +75,12 @@ public class MainActivity extends Activity {
 	
 	//boton SMS
 	Button buttonSMS;
-	///////////////////////////////////
+	
+	
    
   
-  @Override
+ 
+@Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
  
@@ -90,44 +103,116 @@ public class MainActivity extends Activity {
     //btnOn = (Button) findViewById(R.id.btnOn);					// button LED ON
     //btnOff = (Button) findViewById(R.id.btnOff);				// button LED OFF
     txtArduino = (TextView) findViewById(R.id.txtArduino);		// for display the received data from the Arduino
-    txtArduino.setText("Data from Arduino: no leemos");
-    h = new Handler() {
-    	public void handleMessage(Message msg) {
-    		switch (msg.what) {
-            case RECIEVE_MESSAGE:													// if receive massage
-            	txtArduino.setText("Data from Arduino: entra a leer");
-            	byte[] readBuf = (byte[]) msg.obj;
-            	String strIncom = new String(readBuf, 0, msg.arg1);					// create string from bytes array
-            	sb.append(strIncom);												// append string
-            	int endOfLineIndex = sb.indexOf("\r\n");							// determine the end-of-line
-            	if (endOfLineIndex > 0) { 											// if end-of-line,
-            		String sbprint = sb.substring(0, endOfLineIndex);				// extract string
-                    sb.delete(0, sb.length());										// and clear
-                	txtArduino.setText("Data from Arduino: " + sbprint); 	        // update TextView
-                 
-                }
-            	//Log.d(TAG, "...String:"+ sb.toString() +  "Byte:" + msg.arg1 + "...");
-            	break;
-    		}
-        };
-	};
-     
-    btAdapter = BluetoothAdapter.getDefaultAdapter();		// get Bluetooth adapter
-    checkBTState();
-
     
-  //Comprobamos si el GPS esta encendido o apagado
-  		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-  	    if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-  	        Toast.makeText(this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
-  	    }else{
-  	        showGPSDisabledAlertToUser();
-  	    }
+  
+    
+    //get Bluetooth adapter 
+    bluetooth = BluetoothAdapter.getDefaultAdapter();		
+    //Comprobamos si el BT esta encendido o apagado
+    checkBTState(); 
+    //Comprobamos si el GPS esta encendido o apagado
+    checkGpsState() ;
+    
+    //Inicializacion del Handler
+    inicioHandler();
+    Log.d("ConnectToServerThread", "inicializo todo");
+    
+    
+    
   }
   
-  /////////////////////////////////////////
-  //Metodos mios
-  ///////////////////////////////////////////////////////////////////////
+  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+//Incializar Handler
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+  private void inicioHandler(){
+	  puente = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+				txtArduino.setText(msg.obj.toString());
+			}
+		};
+  }
+  
+  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Comprobar estado del BT
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private void checkBTState() {
+		//
+		if (bluetooth == null) {
+			System.out.println("Device do not suportt");
+			Toast.makeText(this, "Device does not support Bluetooth",
+					Toast.LENGTH_LONG).show();
+		}
+
+		String toastText;
+		if (bluetooth.isEnabled()) {
+			String address = bluetooth.getAddress();
+			String name = bluetooth.getName();
+			toastText = name + " : " + address;
+			
+		} else {
+			toastText = "Bluetooth disabled";
+		}
+		System.out.println(toastText);
+		Toast.makeText(this, toastText, Toast.LENGTH_LONG).show();
+
+		Set<BluetoothDevice> bondedDevices = bluetooth.getBondedDevices();
+		BluetoothDevice myDevice = null;
+		for (BluetoothDevice bluetoothDevice : bondedDevices) {
+			if (bluetoothDevice.getName().equals("HC-05")) {
+				myDevice = bluetoothDevice;
+				Toast.makeText(this, "Encontrado!", Toast.LENGTH_SHORT).show();
+				break;
+			}
+		}
+		if (myDevice == null) {
+			Toast.makeText(this, "No paired device found", Toast.LENGTH_SHORT)
+					.show();
+			return;
+		}
+		
+		
+		try {
+			System.out.println("conectando");
+			clientSocket = myDevice.createRfcommSocketToServiceRecord(uuid);
+			clientSocket.connect();
+			System.out.println("enviando");
+			DataOutputStream outputChannel =
+			new DataOutputStream(clientSocket.getOutputStream());
+			outputChannel.writeChars("\nConectado al dispositivo android E \r\n");
+			outputChannel.flush();
+			
+			new RecibirComando(clientSocket).start();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Toast.makeText(this, "Conectado!", Toast.LENGTH_SHORT).show();
+	}
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+  //Comprobar estado GPS
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private void checkGpsState() {
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+  	    	if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+  	    		Toast.makeText(this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
+  	    	}else{
+  	    		showGPSDisabledAlertToUser();
+  	    	}
+	}
+	
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Mensaje que se visualiza para permitir el encendido del GPS
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   private void showGPSDisabledAlertToUser(){
 		new AlertDialog.Builder(this)
@@ -158,36 +243,36 @@ public class MainActivity extends Activity {
 
 private void comenzarLocalizacion()
 {
-
-//Obtenemos una referencia al LocationManager
-locManager = 
-(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
-//Obtenemos la última posición conocida
-Location loc = 
-locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-//Mostramos la última posición conocida
-mostrarPosicion(loc);
-
-//Nos registramos para recibir actualizaciones de la posición
-locListener = new LocationListener() {
-public void onLocationChanged(Location location) {
-mostrarPosicion(location);
-}
-public void onProviderDisabled(String provider){
-lblEstado.setText("Provider OFF");
-}
-public void onProviderEnabled(String provider){
-lblEstado.setText("Provider ON ");
-}
-//Actualizamos la posicion del GPS cada 30 segundos
-public void onStatusChanged(String provider, int status, Bundle extras){
-}
-};
-
-locManager.requestLocationUpdates(
-LocationManager.GPS_PROVIDER, 30, 0, locListener);
+	
+	//Obtenemos una referencia al LocationManager
+	locManager = 
+	(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	
+	//Obtenemos la última posición conocida
+	Location loc = 
+	locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	
+	//Mostramos la última posición conocida
+	mostrarPosicion(loc);
+	
+	//Nos registramos para recibir actualizaciones de la posición
+	locListener = new LocationListener() {
+		public void onLocationChanged(Location location) {
+			mostrarPosicion(location);
+		}
+		public void onProviderDisabled(String provider){
+			lblEstado.setText("Provider OFF");
+		}
+		public void onProviderEnabled(String provider){
+			lblEstado.setText("Provider ON ");
+		}
+		//Actualizamos la posicion del GPS cada 30 segundos
+		public void onStatusChanged(String provider, int status, Bundle extras){
+		}
+	};
+	
+	locManager.requestLocationUpdates(
+	LocationManager.GPS_PROVIDER, 30, 0, locListener);
 }
 
 
@@ -196,18 +281,18 @@ private void mostrarPosicion(Location loc) {
 * contrario nos muestra el mensaje sin datos para latitud y longitud
 * en caso contrario nos muestra la posición en la que nos encontramos
 */
-if(loc != null)
-{
-lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
-lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
-lblPrecision.setText("Precision: " + String.valueOf(loc.getAccuracy()));
-}
-else
-{
-lblLatitud.setText("Latitud: (sin_datos)");
-lblLongitud.setText("Longitud: (sin_datos)");
-lblPrecision.setText("Precision: (sin_datos)");
-}
+	if(loc != null)
+		{
+		lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
+		lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
+		lblPrecision.setText("Precision: " + String.valueOf(loc.getAccuracy()));
+		}
+	else
+		{
+		lblLatitud.setText("Latitud: (sin_datos)");
+		lblLongitud.setText("Longitud: (sin_datos)");
+		lblPrecision.setText("Precision: (sin_datos)");
+		}
 }
 
 
@@ -233,149 +318,83 @@ super.onStop();
 
 }
   
-  
-  ////////////////////////////////////////////////////////////////////////
-  
-  private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-      if(Build.VERSION.SDK_INT >= 10){
-          try {
-              final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
-              return (BluetoothSocket) m.invoke(device, MY_UUID);
-          } catch (Exception e) {
-              Log.e(TAG, "Could not create Insecure RFComm Connection",e);
-          }
-      }
-      return  device.createRfcommSocketToServiceRecord(MY_UUID);
-  }
-   
+ 
+
+
   @Override
   public void onResume() {
     super.onResume();
-    comenzarLocalizacion();
-    Log.d(TAG, "...onResume - try connect...");
-   
-    // Set up a pointer to the remote node using it's address.
-    BluetoothDevice device = btAdapter.getRemoteDevice(address);
-   
-    // Two things are needed to make a connection:
-    //   A MAC address, which we got above.
-    //   A Service ID or UUID.  In this case we are using the
-    //     UUID for SPP.
-    /*
-	try {
-		btSocket = createBluetoothSocket(device);
-	} catch (IOException e) {
-		errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-	}*/
     
-    try {
     
-      btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-    } catch (IOException e) {
-      errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-    }
-   
-    // Discovery is resource intensive.  Make sure it isn't going on
-    // when you attempt to connect and pass your message.
-    btAdapter.cancelDiscovery();
-   
-    // Establish the connection.  This will block until it connects.
-    Log.d(TAG, "...Connecting...");
-    try {
-      btSocket.connect(); 
-      Log.d(TAG, "....Connection ok...");
-    } catch (IOException e) {
-      try {
-        btSocket.close();
-      } catch (IOException e2) {
-        errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-      }
-    }
-     
-    // Create a data stream so we can talk to server.
-    Log.d(TAG, "...Create Socket...");
-    mConnectedThread = new ConnectedThread(btSocket);
-    mConnectedThread.start();
+    //comenzarLocalizacion();
+    
   }
  
   @Override
   public void onPause() {
-    super.onPause();
+    super.onPause();   
  
-    Log.d(TAG, "...In onPause()...");
-  
-    try     {
-      btSocket.close();
-    } catch (IOException e2) {
-      errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-    }
   }
    
-  private void checkBTState() {
-    // Check for Bluetooth support and then check to make sure it is turned on
-    // Emulator doesn't support Bluetooth and will return null
-    if(btAdapter==null) { 
-      errorExit("Fatal Error", "Bluetooth not support");
-    } else {
-      if (btAdapter.isEnabled()) {
-        Log.d(TAG, "...Bluetooth ON...");
-      } else {
-        //Prompt user to turn on Bluetooth
-        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(enableBtIntent, 1);
-      }
-    }
-  }
- 
-  private void errorExit(String title, String message){
-    Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
-    finish();
-  }
- 
-  private class ConnectedThread extends Thread {
-	    private final InputStream mmInStream;
-	    private final OutputStream mmOutStream;
-	 
-	    public ConnectedThread(BluetoothSocket socket) {
-	        InputStream tmpIn = null;
-	        OutputStream tmpOut = null;
-	 
-	        // Get the input and output streams, using temp objects because
-	        // member streams are final
-	        try {
-	            tmpIn = socket.getInputStream();
-	            tmpOut = socket.getOutputStream();
-	        } catch (IOException e) { }
-	 
-	        mmInStream = tmpIn;
-	        mmOutStream = tmpOut;
-	    }
-	 
-	    public void run() {
-	        byte[] buffer = new byte[256];  // buffer store for the stream
-	        int bytes; // bytes returned from read()
+  
+  
 
-	        // Keep listening to the InputStream until an exception occurs
-	        while (true) {
-	        	try {
-	                // Read from the InputStream
-	                bytes = mmInStream.read(buffer);		// Get number of bytes and message in "buffer"
-                    h.obtainMessage(RECIEVE_MESSAGE, bytes, -1, buffer).sendToTarget();		// Send to message queue Handler
-	            } catch (IOException e) {
-	                break;
-	            }
-	        }
-	    }
-	 
-	    /* Call this from the main activity to send data to the remote device */
-	    public void write(String message) {
-	    	Log.d(TAG, "...Data to send: " + message + "...");
-	    	byte[] msgBuffer = message.getBytes();
-	    	try {
-	            mmOutStream.write(msgBuffer);
-	        } catch (IOException e) {
-	            Log.d(TAG, "...Error data send: " + e.getMessage() + "...");     
-	          }
-	    }
+  
+
+  ///////////////////////////////////////////////////////////////////
+  //Enviar comando a Arduino
+  ///////////////////////////////////////////////////////////////////
+	public void enviarComando(String comando){
+		DataOutputStream outputChannel;
+		try {
+			
+			outputChannel = new DataOutputStream(clientSocket.getOutputStream());
+			outputChannel.writeChars(comando);
+			outputChannel.flush();
+			Log.i("enviando comando", comando);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//Clase encargada de encontrar el dispositivo que queremos, conectarnos a el e inicializar 
+	//la hebra de comunicacion
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+private class RecibirComando extends Thread{
+		
+		BluetoothSocket clientSocket;
+		
+		public RecibirComando(BluetoothSocket client) {
+			clientSocket=client;
+		}
+		
+		@Override
+		public void run() {
+			System.out.println("recibiendo");
+			BufferedReader input;
+			while(true){
+			try {
+				input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				System.out.println("hebra de recepcion");
+				
+				String a= input.readLine();
+				System.out.println("hebra de recepcion final");
+				Message msg = new Message();
+				msg.obj= a;
+				puente.sendMessage(msg);
+				System.out.println(a);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			}
+			
+		}
+		
+		
+	}
+ 
 }
