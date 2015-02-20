6498fb09a6aada96ce94bc0b729fee6e75d3537c
valensr23 <valen-sr@outlook.com>
2015-02-20 11:36:50 +0100

    Android BT perdida
    Accion por si se produce la perdida de señal de BT

--- Android/src/motosafe/app/MainActivity.java
+++ Android/src/motosafe/app/MainActivity.java
@@ -1,6 +1,9 @@
 package motosafe.app;
 
 /*
+ * Proyecto Fin de Carrera, Seguridad en vehículos de dos ruedas.
+ * Codigo Android encargado de la comunicación BT con Arduino y
+ * la impletación de un algoritmo para detectar un posible accidente.
  * @author: Valentín Sánchez Ramírez
  */
 import java.io.BufferedReader;
@@ -9,28 +12,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
 import android.R.string;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -38,45 +34,36 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
  
 public class MainActivity extends Activity {
 
    
-  	//Para el BT
+  	//Inicialización de las varialbes correspondientes a la comunicación Bluetooth
   	BluetoothAdapter bluetooth;
   	Handler puente;
   	BluetoothSocket clientSocket;
   	BluetoothDevice myDevice = null;
-  	//SPP UUID service
+  	
+  	//UUIID que usaremos para el Bluetooth
   	UUID uuid= UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-  
-  
-
-  	// MAC-address de mis dispositivo BT
-  	private static String address = "98:D3:31:B1:CC:72";
-  
-
-  
-	//TextView de la posicion del GPS
+    
+	//TextView´s para visualizar como se comporta el algoritmo y comprobar su estado
 	private TextView lblLatitud;
 	private TextView lblLongitud;
 	private TextView lblPrecision;
 	private TextView lblEstado;
 	
+	//Inicialización de las varialbes correspondientes al GPS
 	private LocationManager locManager;
 	private LocationListener locListener;
 	
-	//boton encendido y apagaado de la app
+	//boton encendido y apagado de la aplicación
 	Button buttonOn;
 	Button buttonOff;
 	
-	
-   
   
  
 @Override
@@ -84,17 +71,16 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
  
     setContentView(R.layout.activity_main);
-    ///////////////////////////////
     
 	lblLatitud = (TextView) findViewById(R.id.LblPosLatitud);
 	lblLongitud = (TextView) findViewById(R.id.LblPosLongitud);
 	lblPrecision = (TextView) findViewById(R.id.LblPosPrecision);
 	lblEstado = (TextView) findViewById(R.id.LblEstado);
-	//inicializo button y fijamos la accion que queremos que desarrolle
+	//inicializo button y fijamos la accion que queremos que ejecute
 	buttonOn = (Button)findViewById(R.id.buttonOn);
 	buttonOff = (Button)findViewById(R.id.buttonOff);
 	
-	//Metodo encargado del encendido de la app
+	//Método encargado del encendido de la app
 	buttonOn.setOnClickListener(new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -107,7 +93,7 @@ public class MainActivity extends Activity {
 		    
 		    //Inicializacion del Handler
 		    inicioHandler();
-		    Log.d("ConnectToServerThread", "inicializo todo");	
+		    Log.d("OnCreate", "inicializo todo");	
 		}
 	});
     
@@ -127,7 +113,9 @@ public class MainActivity extends Activity {
   
   
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-//Incializar Handler
+//Incializar Handler,
+//permite la lectura de los datos almacenados en el buffer, para posteriormente enviarlos 
+//como una cadena String al metodo operoDatos()
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   private void inicioHandler(){
@@ -137,14 +125,19 @@ public class MainActivity extends Activity {
 				byte[] buffer   = null;
 			    String mensaje  = null;
 			            buffer = (byte[])msg.obj;
+			            //converte a String todo lo leido por buffer.
 			            mensaje = new String(buffer, 0, msg.arg1);
 			            Log.d("mensaje que recibimos:",""+mensaje);
-			            operoDatos(mensaje);
-			            
+			            operoDatos(mensaje);   
 			}
 		};
   }
   
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Metodo operoDatos(), encargado de recibir un String y convertir los números separados
+//por las letras "a", "b", "c" y "d" en datos tipo double, para posteriormente ser evaluados
+//en nuestro algoritmo  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   private void operoDatos(String leido){
 
@@ -152,32 +145,28 @@ public class MainActivity extends Activity {
 	  int pos2 = leido.indexOf("b", pos1);
 	  int pos3 = leido.indexOf("c", pos2);
 	  int pos4 = leido.indexOf("d", pos3);
-	  int pos5 = leido.indexOf(" ");
+	  //conversion de String a double en caso de existir en el mismo String todas la variables buscadas
 	  if(pos1 !=-1 && pos2 !=-1 && pos3!=-1 && pos4!=-1){
-		  //String ax = (leido.substring(pos1+1, pos2));
-		  //String ay = (leido.substring(pos2+1, pos3));
-		  //String az = (leido.substring(pos3+1, pos4));
 		  double x = Double.valueOf(leido.substring(pos1+1, pos2));
 		  double y = Double.valueOf(leido.substring(pos2+1, pos3));
 		  double z = Double.valueOf(leido.substring(pos3+1, pos4));
-		  //Log.d("algo falla aqui x:",""+x);
-		  //Log.d("algo falla aqui y:",""+y);
-		  //Log.d("algo falla aqui z:",""+z);
-		  //lblLongitud.setText(ax);
-		  //lblPrecision.setText(ay);
-		  //lblEstado.setText(az);
+
 		  algoritmoEmergencia(x, y, z);
 	  }
   }
   
-  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Metodo encargado de calcular el ángulo de inclinación de la moto a partir de los datos recibidos
+//anteriormente y procesados. Comprueba si el angulo de inclinación "theta" es superior a 55 grados en
+//ambos lados, en caso de ser afirmativo procedemos a comprobar la velocidad a la que cirulamos.  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   private void algoritmoEmergencia(double x, double y, double z){
 	  double theta;
 	  double phi;
 	  //constante para que pi sea 180 grados
 	  double ang = (180/3.1415);
-	  
+	  //calculo del angulo theta
 	  theta = Math.atan2(x, z)*ang;
 	  phi = Math.atan2(y, z);
 	  lblLatitud.setText("theta:"+theta);
@@ -188,7 +177,12 @@ public class MainActivity extends Activity {
 	  }
   }
   
-  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Metodo encargado de calcular la velocidad a la que circulamos, para ello inicializamos
+//el GPS, el cual nos devolvera la velocidad a la que circulamos. En caso de ser inferior a 
+//3 metros por segundo procedemos a que el GPS nos devuelva nuestra latitud, longitud y
+//precisión del mismo, para posteriormente llamar al método sendSMS().  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   private void checkSpeed(){
 	  lblPrecision.setText("mido velocidad");
@@ -206,49 +200,40 @@ public class MainActivity extends Activity {
 			public void onLocationChanged(Location location) {
 				//tomamos la ubicacion actual y medimos la velocidad a la que nos encontramos
 				float speed = location.getSpeed();
-				
 				if(speed < 3){
 					lblPrecision.setText("me he caido :(");
+					//las variables que devuelve el GPS con la ubicacion son de tipo double
 					double latitud = location.getLatitude();
 					double longitud = location.getLongitude();
 					double precision = location.getAccuracy();
-					sendSMS("112","Me encuentro en", latitud, longitud, precision);
-					
+					sendSMS("000","Me encuentro en", latitud, longitud, precision);
 				}
-				
 			}
 
 			@Override
-			public void onProviderDisabled(String arg0) {
-				// TODO Auto-generated method stub
-				
+			public void onProviderDisabled(String arg0) {	
 			}
 
 			@Override
 			public void onProviderEnabled(String arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 
 			@Override
 			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-				// TODO Auto-generated method stub
-				
 			}
 		};
 		
 		locManager.requestLocationUpdates(
 		LocationManager.GPS_PROVIDER, 30, 0, locListener);
-		
   }
   
-  
-  
-  
-  
+ 
   
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Comprobar estado del BT
+//Metodo encargado de comprobar el estado del Bluetooth y si nuestro dispositivo posee tal.
+//Comprobamos los dispositivos que capta nuestro Bluetooth y si alguno se correcpode con nuestro
+//HC-05 lo asociamos, para posteriormente conectarlo y establecer la comunicación. Ademas inicializamos
+//la recepción de datos.  
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	private void checkBTState() {
@@ -282,8 +267,8 @@ public class MainActivity extends Activity {
 			}
 		}
 		if (myDevice == null) {
-			Toast.makeText(this, "No paired device found", Toast.LENGTH_SHORT)
-					.show();
+			Toast.makeText(this, "No paired device found", Toast.LENGTH_SHORT).show();
+			finish();
 			return;
 		}
 		
@@ -301,15 +286,13 @@ public class MainActivity extends Activity {
 			new RecibirComando(clientSocket).start();
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		Toast.makeText(this, "Conectado!", Toast.LENGTH_SHORT).show();
 	}
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-  //Comprobar estado GPS
+  //Comprobamos el estado GPS, en caso de estar apagado procedemos a encenderlo
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	private void checkGpsState() {
@@ -322,7 +305,8 @@ public class MainActivity extends Activity {
 	}
 	
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Mensaje que se visualiza para permitir el encendido del GPS
+  //metodo encargado de la visualización del mensaje de alerta para encender Bluetooth en nuestro dispositivo,
+  //en caso afirmativo podremos activarlo, en caso contrario la app se cerrara automaticamente.
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   private void showGPSDisabledAlertToUser(){
@@ -349,7 +333,7 @@ public class MainActivity extends Activity {
   
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Metodo envio SMS
+//Metodo envio SMS, el cual envia un SMS al numero de emergias indicándole nuestra posición.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
 int numMensajes = 0;
@@ -360,7 +344,9 @@ private void sendSMS (String phoneNumber, String message, double latitud, double
 	String longitudS = Double.toString(longitud);
 	String precisionS = Double.toString(precision);
 	message = "he sufrido un accidente, me encuentro en latitud:"+latitudS+"longitud:"+longitudS+"precision:"+precisionS;
+	//la varaible numMensajes será la encargada de que solo se envie un 1 SMS y no varios
 	lblEstado.setText(message);
+	//TODO añadir numero al que enviar mensaje "phoneNumber"
 	if(numMensajes !=1){
 		sms.sendTextMessage(phoneNumber, null, message, null, null);
 		numMensajes=1;
@@ -370,45 +356,35 @@ private void sendSMS (String phoneNumber, String message, double latitud, double
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
   
-@Override
-protected void onStop() {
-	/*Cuando la actividad es destruida, se ejecuta este método.
-	 */
-	super.onStop();
-}
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
   
- 
 
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    //comenzarLocalizacion();
-  }
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
  
-  @Override
-  public void onPause() {
-    super.onPause();   
-  }
+	@Override
+	public void onPause() {
+		super.onPause();   
+	}
    
   
-  
 
-  
-
-  ///////////////////////////////////////////////////////////////////
-  //Enviar comando a Arduino
-  ///////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  //Metodo encargado del envio de datos a la placa Arduino
+  ////////////////////////////////////////////////////////////////////////////////////////
 	public void enviarComando(String comando){
 		DataOutputStream outputChannel;
 		try {
-			
 			outputChannel = new DataOutputStream(clientSocket.getOutputStream());
 			outputChannel.writeChars(comando);
 			outputChannel.flush();
 			Log.i("enviando comando", comando);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 				
@@ -424,7 +400,7 @@ private class RecibirComando extends Thread{
 		BluetoothSocket clientSocket;
 		private final InputStream inputStream;    // Flujo de entrada (lecturas)
 	    private final OutputStream outputStream;   // Flujo de salida (escrituras)
-	    
+	    MainActivity mainActivity = new MainActivity();
 		
 		public RecibirComando(BluetoothSocket client) {
 			clientSocket=client;
@@ -447,6 +423,8 @@ private class RecibirComando extends Thread{
 		
 		
 		//Metodo principal del hilo, encargado de realizar las lecturas
+		int numPerdidas=0;
+		int estado = 0;
 		public void run()
 		{
 			byte[] buffer = new byte[1024];
@@ -457,25 +435,32 @@ private class RecibirComando extends Thread{
 			while(true)
 			{
 				try {
+					if (estado ==1){
+						numPerdidas++;
+					}
+					if(numPerdidas==5){
+						mainActivity.checkSpeed();
+					}
+					
 					Thread.sleep(600);
 					// Leemos del flujo de entrada del socket
 					bytes = inputStream.read(buffer);
-					
 					// Enviamos la informacion a la actividad a traves del handler.
 					// El metodo handleMessage sera el encargado de recibir el mensaje
-					// y mostrar los datos recibidos en el TextView
+					// y operar con ellos tal y como le indiquemos
 					puente.obtainMessage(1, bytes, -1, buffer).sendToTarget();
 				}
 				catch(IOException e) {
+					estado = 1;
 					Log.e("Bluetooth", "HiloConexion.run(): Error al realizar la lectura", e);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 
+//Fin
 }
 		
 		
